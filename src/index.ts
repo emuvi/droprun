ac14a29d34287fb39440c616a39eab56cfa2800f
Éverton M. Vieira <emuvi@outlook.com.br>
2022-06-22 12:57:58 -0300

    implements he basic features on the new apis

--- src/index.ts
+++ src/index.ts
@@ -11,50 +11,67 @@ import {
 import { QinSoul } from "qinpel-res";
 
 class DropRun extends QinColumn {
-  private qinExplorer = new QinFileView();
+  private _qinExplorer = new QinFileView();
 
-  private qinMiddle = new QinLine();
-  private qinUp = new QinButton({ icon: new QinIcon(QinAsset.FaceArrowUp) });
-  private qinFolder = new QinString();
-  private qinDrop = new QinButton({ icon: new QinIcon(QinAsset.FaceArrowDown) });
+  private _qinUp = new QinButton({ icon: new QinIcon(QinAsset.FaceArrowUp) });
+  private _qinFolder = new QinString();
+  private _qinDrop = new QinButton({ icon: new QinIcon(QinAsset.FaceArrowDown) });
+  private _qinFind = new QinLine({ items: [this._qinUp, this._qinFolder, this._qinDrop] });
 
-  private qinBottom = new QinLine();
-  private qinCommands = new QinCombo();
-  private qinParameters = new QinString();
-  private qinWildcard = new QinButton({ icon: new QinIcon(QinAsset.FaceCog) });
+  private _qinCommands = new QinCombo();
+  private _qinParameters = new QinString();
+  private _qinWildcard = new QinButton({ icon: new QinIcon(QinAsset.FaceCog) });
+  private _qinMount = new QinLine({
+    items: [this._qinCommands, this._qinParameters, this._qinWildcard],
+  });
+
+  private _qinTokens = new QinCombo();
+  private _qinAsk = new QinButton({ icon: new QinIcon(QinAsset.FaceEye) });
+  private _qinRunning = new QinLine({ items: [this._qinTokens, this._qinAsk] });
 
   public constructor() {
     super();
-    this.qinExplorer.install(this);
-    this.qinExplorer.style.putAsFlexMax();
+    this.initExplorer();
+    this.initFind();
+    this.initMount();
+    this.initRunning();
+  }
 
-    this.qinMiddle.install(this);
-    this.qinUp.install(this.qinMiddle);
-    this.qinUp.addActionMain((_) => {
-      this.qinExplorer.goFolderUp((folder) => {
-        this.qinFolder.value = folder;
+  private initRunning() {
+    this._qinRunning.install(this);
+    this._qinTokens.style.putAsFlexMax();
+  }
+
+  private initExplorer() {
+    this._qinExplorer.install(this);
+    this._qinExplorer.style.putAsFlexMax();
+  }
+
+  private initFind() {
+    this._qinFind.install(this);
+    this._qinUp.addActionMain((_) => {
+      this._qinExplorer.goFolderUp((folder) => {
+        this._qinFolder.value = folder;
       });
     });
-    this.qinFolder.install(this.qinMiddle);
-    this.qinFolder.style.putAsFlexMax();
-    this.qinDrop.install(this.qinMiddle);
-    this.qinDrop.addActionMain((_) => {
-      this.dropSelectedAndRun();
-    });
-    this.qinFolder.addAction((qinEvent) => {
+    this._qinFolder.style.putAsFlexMax();
+    this._qinFolder.addAction((qinEvent) => {
       if (qinEvent.isEnter) {
         this.loadExplorer();
       }
     });
-    this.qinBottom.install(this);
-    this.qinCommands.install(this.qinBottom);
-    this.qinParameters.install(this.qinBottom);
-    this.qinParameters.style.putAsFlexMax();
-    this.qinWildcard.install(this.qinBottom);
-    this.qinWildcard.addActionMain((_) => {
-      this.qinParameters.insertAtCursor("$selected$");
+    this._qinDrop.addActionMain((_) => {
+      this.dropSelectedAndRun();
     });
+  }
+
+  private initMount() {
+    this._qinMount.install(this);
     this.loadCmds();
+    this._qinParameters.style.putAsFlexMax();
+    this._qinWildcard.addActionMain((_) => {
+      this._qinParameters.insertAtCursor("%FILE%");
+    });
   }
 
   private loadCmds() {
@@ -63,7 +80,7 @@ class DropRun extends QinColumn {
       .then((res) => {
         let cmds = QinSoul.body.getTextLines(res.data);
         for (const cmd of cmds) {
-          this.qinCommands.addItem({ title: cmd, value: cmd });
+          this._qinCommands.addItem({ title: cmd, value: cmd });
         }
       })
       .catch((err) => {
@@ -72,30 +89,31 @@ class DropRun extends QinColumn {
   }
 
   public loadExplorer() {
-    this.qinExplorer.load(this.qinFolder.value, (serverFolder) => {
-      this.qinFolder.value = serverFolder;
+    this._qinExplorer.load(this._qinFolder.value, (serverFolder) => {
+      this._qinFolder.value = serverFolder;
     });
   }
 
   public dropSelectedAndRun() {
-    let command = this.qinCommands.value;
-    let parameters = QinSoul.body.parseParameters(this.qinParameters.value);
-    let selected = this.qinExplorer.value;
+    let command = this._qinCommands.value;
+    let parameters = QinSoul.body.parseParameters(this._qinParameters.value);
+    let selected = this._qinExplorer.value;
     for (const item of selected) {
       let item_params = [];
       for (const param of parameters) {
-        item_params.push(param.replace("$selected$", item));
+        item_params.push(param.replace("%FILE%", item));
       }
       this.qinpel.talk
-        .post("/run/cmd/" + command, {
-          params: item_params,
+        .post("/cmd/run", {
+          exec: command,
+          args: item_params,
           inputs: [],
         })
         .then((res) => {
-          // [ TODO ]
+          this._qinTokens.addSame(res.data);
         })
         .catch((err) => {
-          // [ TODO ]
+          this.qinpel.jobbed.statusError(err, "{droprun}(ErrCode-000001)");
         });
     }
   }
